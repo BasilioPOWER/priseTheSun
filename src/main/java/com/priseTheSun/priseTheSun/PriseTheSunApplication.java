a003a078e416fb7051ec53a81daa53f0362f1cfe
Gabriel Basilio Souza <gb.darkaico@gmail.com>
2023-10-24 14:43:43 -0300

    Projeto Base
    Criado apartir do https://start.spring.io/

---
+++ src/main/java/com/priseTheSun/priseTheSun/PriseTheSunApplication.java
@@ -0,0 +1,13 @@
+package com.priseTheSun.priseTheSun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriseTheSunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriseTheSunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priseTheSun/priseTheSun/PriseTheSunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priseTheSun.priseTheSun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriseTheSunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
